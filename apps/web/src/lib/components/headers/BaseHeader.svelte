c8f5cf0afee11dd6aafa6c0bccaba3e46e9e352b
David Majdandžić <dmajdandzic@neos.hr>
2023-06-17 00:57:05 +0200

    Update template

--- apps/web/src/lib/components/headers/BaseHeader.svelte
+++ apps/web/src/lib/components/headers/BaseHeader.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import ThemeToggle from '$components/theme/ThemeToggle.svelte';
+    import {faXmark} from '@fortawesome/free-solid-svg-icons';
+    import {exit} from '@tauri-apps/api/process';
+    import Fa from 'svelte-fa';
+
+    Fa;
 </script>
 
 <div class="flex h-20 items-center justify-between px-4" data-tauri-drag-region>
@@ -14,7 +19,10 @@
         </ul>
     </nav>
 
-    <div class="flex items-center space-x-4 [&>*:hover]:text-primary [&>*]:transition-all [&>*]:duration-300">
+    <div class="[&>*:hover]:text-primary flex items-center space-x-4 [&>*]:transition-all [&>*]:duration-300">
         <ThemeToggle/>
+        <button class="p-4" on:click={() => exit(0)}>
+            <Fa icon={faXmark} size="1.5x"/>
+        </button>
     </div>
 </div>
